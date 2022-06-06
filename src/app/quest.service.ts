43bff7eb5528eff11e7c8e19594de7748aaad86b
Rune Hartvig <rune_hartvig@hotmail.com>
2022-06-06 20:15:46 +0200

    add errorService and move error handling

--- src/app/quest.service.ts
+++ src/app/quest.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, tap, catchError, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { Quest } from './quest';
+import { ErrorService } from './shared/error.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,39 +17,21 @@ export class QuestService {
 
   constructor(
     private http: HttpClient,
+    private errorService: ErrorService,
     private messageService: MessageService) { }
 
   /** GET quests from the server */
   getQuests(): Observable<Quest[]> {
     return this.http.get<Quest[]>(this.questsUrl)
       .pipe(
-        tap(_ => this.log('fetched heroes')),
-        catchError(this.handleError<Quest[]>('getQuests', []))
+        tap(_ => this.log('fetched quests')),
+        catchError(this.errorService.handleError<Quest[]>('getQuests', []))
       );
-  }
-  /**
-  * Handle Http operation that failed.
-  * Let the app continue.
-  *
-  * @param operation - name of the operation that failed
-  * @param result - optional value to return as the observable result
-  */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
   }
 
   /** Log a QuestService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`QuestService: ${message}`);
   }
+
 }
---
+++ src/app/shared/error.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MessageService } from '../message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ErrorService {
+
+  constructor(private messageService: MessageService) { }
+
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  *
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log an error message with the MessageService */
+  log(message: string) {
+    this.messageService.add(`Error: ${message}`);
+  }
+}
