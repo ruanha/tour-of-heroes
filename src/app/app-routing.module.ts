19971757e1516e7c4a012403139823718b397ce4
Rune Hartvig <rune_hartvig@hotmail.com>
2022-06-06 16:46:28 +0200

    add link and routing to quest-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
 import { QuestsComponent } from './quests/quests.component';
+import { QuestDetailsComponent } from './quest-details/quest-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailsComponent },
-  { path: 'quests', component: QuestsComponent}
+  { path: 'quests', component: QuestsComponent},
+  { path: 'quests/:id', component: QuestDetailsComponent}
 ];
 
 @NgModule({
